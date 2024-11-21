b3d4af7d875f564f69b79a5085e5c662b34cd9d1
Lucas <medinalucasian@gmail.com>
2024-11-21 19:00:11 -0300

    new pages to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { CalendarPageComponent } from './pages/calendar-page/calendar-page.component';
 
 export const routes: Routes = [
-    {path:'', component:LandingPageComponent}
+    {path:'', component:LandingPageComponent},
+    {path:'dashboard', component:DashboardPageComponent},
+    {path:'calendar', component:CalendarPageComponent}
 ];
