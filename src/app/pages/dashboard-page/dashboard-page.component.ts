c00766dea7b57c4a5a7ec1cee48ff0820510ec71
Lucas <medinalucasian@gmail.com>
2024-11-22 00:07:56 -0300

    calendar component created in development

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header/header.component";
+import { CalendarComponent } from "../../components/calendar/calendar.component";
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CalendarComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
